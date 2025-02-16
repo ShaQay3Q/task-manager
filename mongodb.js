3977a458998be6b23f5690cf3006c11b0c5cc640
ShaQayeQ <shaghay3gh@gmail.com>
2025-02-16 17:56:17 +0100

    modification on docker-compose.yaml

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,9 @@
-const mongodb = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
 // gonna give access to the neccessary function to connect to the DB to performing CRUD operations
-const MongoClient = mongodb.MongoClient;
+// const MongoClient = mongodb.MongoClient;
 
 const connectionURL = "mongodb://127.0.0.1:27017"; //127.0.0.1 akka localhost => it's beter to use th ip cause localhost slows down the connection
-const databaseName = "task-manager";
+const databaseName = "task-manager"; // It will be generated automatically by MongoDB
 
 MongoClient.connect(
 	connectionURL,
@@ -18,5 +18,22 @@ MongoClient.connect(
 			// );
 		}
 		console.log("Connected corerctly!");
+
+		// database refrence
+		const db = client.db(databaseName); // pass down the name of DB we wanna manipulate (databaseName)
+		// const users = db.collection("users"); // gets: name of the collection
+
+		// users.insertOne({
+		// 	// pass down an object
+		// 	firstName: "Jone",
+		// 	lastName: "Doe",
+		// 	email: "jone.doe@mail.com",
+		// });
+
+		db.collection("users").insert({
+			firstName: "Jone",
+			lastName: "Doe",
+			email: "jone.doe@mail.com",
+		});
 	}
 );
