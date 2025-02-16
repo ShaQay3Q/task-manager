edeeacf5bf9b03f73dd85f98957d1704ef8a117d
ShaQayeQ <shaghay3gh@gmail.com>
2025-02-16 18:37:26 +0100

    yeay... created a new collection

--- mongodb.js
+++ mongodb.js
@@ -1,39 +1,59 @@
-const MongoClient = require("mongodb").MongoClient;
-// gonna give access to the neccessary function to connect to the DB to performing CRUD operations
-// const MongoClient = mongodb.MongoClient;
+const { MongoClient } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017"; //127.0.0.1 akka localhost => it's beter to use th ip cause localhost slows down the connection
+const client = new MongoClient(connectionURL);
 const databaseName = "task-manager"; // It will be generated automatically by MongoDB
 
-MongoClient.connect(
-	connectionURL,
-	{ useNewUrlParser: true },
-	(error, client) => {
-		// this will be called when we connect to the db
-		if (error) {
-			return console.log("Unable to connect to database!");
+async function main() {
+	// Use connect method to connect to the server
+	await client.connect();
+	console.log("Connected successfully to server");
+	const db = client.db(databaseName);
+	const usersCollection = db.collection("users");
+	// console.log(typeof usersCollection);
+	// const collection = db.collection("documents");
 
-			// throw new Error(
-			//     "Unable to connect to database"
-			// );
-		}
-		console.log("Connected corerctly!");
+	const insertResult = await usersCollection.insertOne({ a: 1, b: 2, c: 3 });
+	console.log("Inserted documents =>", insertResult);
+	// the following code examples can be pasted here...
 
-		// database refrence
-		const db = client.db(databaseName); // pass down the name of DB we wanna manipulate (databaseName)
-		// const users = db.collection("users"); // gets: name of the collection
+	return "done.";
+}
 
-		// users.insertOne({
-		// 	// pass down an object
-		// 	firstName: "Jone",
-		// 	lastName: "Doe",
-		// 	email: "jone.doe@mail.com",
-		// });
+main()
+	.then(console.log)
+	.catch(console.error)
+	.finally(() => client.close());
 
-		db.collection("users").insert({
-			firstName: "Jone",
-			lastName: "Doe",
-			email: "jone.doe@mail.com",
-		});
-	}
-);
+// MongoClient.connect(
+// 	connectionURL,
+// 	{ useNewUrlParser: true },
+// 	(error, client) => {
+// 		// this will be called when we connect to the db
+// 		if (error) {
+// 			return console.log("Unable to connect to database!");
+
+// 			// throw new Error(
+// 			//     "Unable to connect to database"
+// 			// );
+// 		}
+// 		console.log("Connected corerctly!");
+
+// 		// database refrence
+// 		const db = client.db(databaseName); // pass down the name of DB we wanna manipulate (databaseName)
+// 		// const users = db.collection("users"); // gets: name of the collection
+
+// 		// users.insertOne({
+// 		// 	// pass down an object
+// 		// 	firstName: "Jone",
+// 		// 	lastName: "Doe",
+// 		// 	email: "jone.doe@mail.com",
+// 		// });
+
+// 		db.collection("users").insert({
+// 			firstName: "Jone",
+// 			lastName: "Doe",
+// 			email: "jone.doe@mail.com",
+// 		});
+// 	}
+// );
